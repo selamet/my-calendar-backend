6927fd6fe401df1898aac1797e9a035ea0bd3ba7
selamet <selamet96@gmail.com>
2020-11-19 20:38:14 +0300

    Add event's model to admin panel

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from event.models import Event
+
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uuid', 'user')
+    list_filter = ['user']
+
+
+admin.site.register(Event, EventAdmin)
