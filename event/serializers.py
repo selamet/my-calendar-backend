413a0d15b6791a9b7df5085b485faf259b981b4c
selamet <selamet96@gmail.com>
2020-11-20 22:30:53 +0300

    use to_representation method for date

--- event/serializers.py
+++ event/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from event.models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Event
         fields = ['uuid', 'title', 'content', 'date', 'flag']
@@ -13,6 +15,17 @@ class EventSerializer(serializers.ModelSerializer):
             "date": {'required': True},
         }
         lookup_field = 'uuid'
+
+    def to_representation(self, obj):
+        data = super(EventSerializer, self).to_representation(obj)
+        data['date'] = {
+            'year': obj.date.year,
+            'month': obj.date.month,
+            'day': obj.date.day,
+            'hour': obj.date.hour,
+            'minute': obj.date.minute,
+        }
+        return data
 
     def create(self, validated_data):
         request = self.context['request']
