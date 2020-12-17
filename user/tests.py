f6a2c298372d7e62259f23859083b813823ff28e
selamet <selamet96@gmail.com>
2020-12-17 19:41:54 +0300

    add login test case

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 
@@ -22,3 +25,26 @@ class UserRegistrationTestCase(APITestCase):
         }
         response = self.client.post(self.url, data)
         self.assertEqual(400, response.status_code)
+
+
+class UserLoginTestCase(APITestCase):
+    url_login = reverse("user:token_obtain_pair")
+
+    def setUp(self):
+        self.username = 'selamet@mail.com'
+        self.password = "qwe123123"
+        self.user = User.objects.create_user(username=self.username, email=self.username, password=self.password)
+
+    def test_user_token(self):
+        response = self.client.post(self.url_login, {'username': 'selamet@mail.com', 'password': 'qwe123123'})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+
+    def test_user_invalid_data(self):
+        response = self.client.post(self.url_login, {'username': 'selamet_hatali@mail.com', 'password': 'qwe123123'})
+        self.assertEqual(401, response.status_code)
+
+    def test_user_empty_data(self):
+        response = self.client.post(self.url_login, {'username': '', 'password': ''})
+        self.assertEqual(400, response.status_code)
+
